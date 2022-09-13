a0a36679a892f85eb065e9fe3e6a544bb06e4b7e
Saki Nakazato <snkzt@mercari.com>
2022-09-13 22:37:49 +0900

    Add unit test

--- example/hello.go
+++ example/hello.go
@@ -3,9 +3,11 @@ package main
 import (
   "fmt"
   "github.com/snkzt/first-go-unit-test/morestrings"
+  "github.com/google/go-cmp/cmp"
 )
 
 func main() {
   fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+  fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module first-go-unit-test
+module github.com/snkzt/first-go-unit-test
 
 go 1.19
+
+require github.com/google/go-cmp v0.5.9
